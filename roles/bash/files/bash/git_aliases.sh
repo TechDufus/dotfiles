2a94ebb5d8dc708394e598d7a2f58fa2a4afffbf
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2022-12-13 12:57:01 -0600

    adding nala role

--- roles/bash/files/bash/git_aliases.sh
+++ roles/bash/files/bash/git_aliases.sh
@@ -8,9 +8,5 @@ alias gc="git checkout"
 
 alias ggl='git log --oneline --graph'
 alias gcb="git checkout -b"
-__git_complete gc _git_checkout
-__git_complete gcb _git_checkout
-
 alias gp="git push"
 alias gpf="git push --force-with-lease"
-__git_complete gp _git_push

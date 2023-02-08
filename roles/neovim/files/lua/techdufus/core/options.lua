d94717894027a5456606937181c24b7c47795ed2
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2023-02-08 10:35:13 -0600

    edit nvim cursor

--- roles/neovim/files/lua/techdufus/core/options.lua
+++ roles/neovim/files/lua/techdufus/core/options.lua
@@ -42,6 +42,7 @@ local options = {
     foldlevelstart = 99,
     -- guifont = "monospace:h17",               -- the font used in graphical neovim applications
     errorbells = false, -- no error bells
+    guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 }
 vim.opt.shortmess:append "c"
 for k, v in pairs(options) do

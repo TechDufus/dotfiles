83245d401cf7256029ff2ba79df7f5193a0b1a95
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2023-03-13 07:55:12 -0500

    changing scheme

--- roles/neovim/files/lua/techdufus/plugins/color_scheme.lua
+++ roles/neovim/files/lua/techdufus/plugins/color_scheme.lua
@@ -1,10 +1,20 @@
 -- Theme Settings
+-- vim.g.catppuccin_flavour = "mocha"
+-- require('catppuccin').setup({
+--   transparent_background = true,
+--   term_colors = true,
+-- })
+-- require('onedark').setup {
+--     style = 'darker',
+--     transparent = true,
+-- }
+-- require('onedark').load()
 require("bluloco").setup({
   style = "auto",               -- "auto" | "dark" | "light"
   transparent = true,
   italics = false,
   terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
-  guicursor = true
 })
 
 vim.cmd('colorscheme bluloco')
+-- vim.cmd [[colorscheme catppuccin]]

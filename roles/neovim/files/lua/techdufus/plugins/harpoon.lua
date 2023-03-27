b7f0e1f5929effe0283312dae9903f0aa99b7361
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2023-03-27 13:18:32 -0500

    adding changes

--- roles/neovim/files/lua/techdufus/plugins/harpoon.lua
+++ roles/neovim/files/lua/techdufus/plugins/harpoon.lua
@@ -1,10 +1,10 @@
 return {
     "ThePrimeagen/harpoon",
     config = function()
-        require("harpoon").setup({
+        require("harpoon").setup {
             menu = {
                 width = vim.api.nvim_win_get_width(0) - 20,
             }
-        })
+        }
     end
 }

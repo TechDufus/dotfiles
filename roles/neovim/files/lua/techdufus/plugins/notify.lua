5588252fb31f83a9b1da28fbde89cafa2ec917b1
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2023-03-31 15:08:58 -0500

    lowering timeout

--- roles/neovim/files/lua/techdufus/plugins/notify.lua
+++ roles/neovim/files/lua/techdufus/plugins/notify.lua
@@ -4,7 +4,7 @@ return {
         require("notify").setup {
             stages = 'fade_in_slide_out',
             background_colour = 'FloatShadow',
-            timeout = 3000,
+            timeout = 2000,
         }
         vim.notify = require("notify")
     end

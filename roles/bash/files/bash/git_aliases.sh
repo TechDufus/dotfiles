ff63674cdeacfed4eb5234d3ebb07dcb08cc81a8
TechDufus <46715299+TechDufus@users.noreply.github.com>
2023-05-08 07:27:37 -0500

    bash git (#35)
    * adding fancier git log
    
    * dufus fix..

--- roles/bash/files/bash/git_aliases.sh
+++ roles/bash/files/bash/git_aliases.sh
@@ -6,7 +6,8 @@ alias gs='git status'
 alias gcane='git commit --amend --no-edit'
 alias gc="git checkout"
 
-alias ggl='git log --oneline --graph'
+alias ggl="git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all"
+
 alias gcb="git checkout -b"
 alias gp="git push"
 alias gpf="git push --force-with-lease"

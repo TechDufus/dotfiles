d0d48c42d5976861a83f10e512552a3a34a03ade
TechDufus <matthewjdegarmo@gmail.com>
2023-05-08 07:26:25 -0500

    dufus fix..

--- roles/bash/files/bash/git_aliases.sh
+++ roles/bash/files/bash/git_aliases.sh
@@ -6,7 +6,7 @@ alias gs='git status'
 alias gcane='git commit --amend --no-edit'
 alias gc="git checkout"
 
-alias ggl="git log --glaph --format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all"
+alias ggl="git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all"
 
 alias gcb="git checkout -b"
 alias gp="git push"

121abc653b8470a75c6ad82387eb675801a6bfc0
TechDufus <46715299+TechDufus@users.noreply.github.com>
2023-05-22 20:00:55 -0500

    why (#37)

--- roles/neovim/files/lua/techdufus/plugins/neo-tree.lua
+++ roles/neovim/files/lua/techdufus/plugins/neo-tree.lua
@@ -17,11 +17,7 @@ return {
     config = function()
         local icons = require('techdufus.core.icons')
         require("neo-tree").setup({
-<<<<<<< HEAD
-            close_if_last_window = false,
-=======
             close_if_last_window = true,
->>>>>>> d85362d (why)
             popup_border_style = "single",
             enable_git_status = true,
             enable_modified_markers = true,

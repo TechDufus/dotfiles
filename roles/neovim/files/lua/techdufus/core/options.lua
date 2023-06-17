548cbfa809d5b1884f343a166aedb98cd4e2aebe
TechDufus <matthewjdegarmo@gmail.com>
2023-06-16 23:26:12 -0500

    adding font magic

--- roles/neovim/files/lua/techdufus/core/options.lua
+++ roles/neovim/files/lua/techdufus/core/options.lua
@@ -39,7 +39,8 @@ local options = {
     sidescrolloff = 8,
     foldmethod = "syntax",
     foldlevelstart = 99,
-    guifont = "monospace:h17", -- the font used in graphical neovim applications
+    -- guifont = "monospace:h17", -- the font used in graphical neovim applications
+    guifont = "Berkeley Mono:h17",
     errorbells = false,        -- no error bells
     guicursor =
     "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"

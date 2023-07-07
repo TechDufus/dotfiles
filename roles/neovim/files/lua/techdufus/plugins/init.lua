a74c8215924d8db73cde6562a897d2d5fb1c599e
TechDufus <46715299+TechDufus@users.noreply.github.com>
2023-07-07 14:50:55 -0500

    alacritty and winget (#54)
    * Adding alacritty and winget role for additional winget installs.
    
    * moving to files
    
    * more config
    
    * comments
    
    * adding padding comment

--- roles/neovim/files/lua/techdufus/plugins/init.lua
+++ roles/neovim/files/lua/techdufus/plugins/init.lua
@@ -46,16 +46,4 @@ return {
     },
     'crispgm/nvim-go',
     'erikzaadi/vim-ansible-yaml',
-    {
-        "jackMort/ChatGPT.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("chatgpt").setup()
-        end,
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim"
-        }
-    },
 }

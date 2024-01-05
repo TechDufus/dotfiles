19e2bc7a30e8e7bb00e8ffa636e8624ad923fbeb
TechDufus <matthewjdegarmo@gmail.com>
2024-01-05 12:12:31 -0600

    Adding ChatGPT plugin for neovim.

---
+++ roles/neovim/files/lua/plugins/chatgpt.lua
@@ -0,0 +1,12 @@
+return {
+    "jackMort/ChatGPT.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+        event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+}

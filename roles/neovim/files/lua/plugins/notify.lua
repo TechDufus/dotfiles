4e5cce943d011ea16116755db4577e6631fd0779
TechDufus <matthewjdegarmo@gmail.com>
2024-01-16 08:46:27 -0600

    adding noice plugin

--- roles/neovim/files/lua/plugins/notify.lua
+++ roles/neovim/files/lua/plugins/notify.lua
@@ -1,11 +1,11 @@
 return {
-    "rcarriga/nvim-notify", -- pretty notifications
-    config = function()
-        require("notify").setup {
-            stages = 'fade_in_slide_out',
-            background_colour = 'FloatShadow',
-            timeout = 2000,
-        }
-        vim.notify = require("notify")
-    end
+    -- "rcarriga/nvim-notify", -- pretty notifications
+    -- config = function()
+    --     require("notify").setup {
+    --         stages = 'fade_in_slide_out',
+    --         background_colour = 'FloatShadow',
+    --         timeout = 2000,
+    --     }
+    --     vim.notify = require("notify")
+    -- end
 }

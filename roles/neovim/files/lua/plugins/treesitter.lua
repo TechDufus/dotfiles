d3bfdea455bf3c02f2b7eae63e478606b5354443
TechDufus <matthewjdegarmo@gmail.com>
2024-01-21 22:24:44 -0600

    This is a bulk config update.

--- roles/neovim/files/lua/plugins/treesitter.lua
+++ roles/neovim/files/lua/plugins/treesitter.lua
@@ -1,20 +1,29 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    -- config = function()
-    --     require 'nvim-treesitter.configs'.setup {
-    --         -- A list of parser names, or "all"
-    --         ensure_installed = "all",
-    --         -- Install parsers synchronously (only applied to `ensure_installed`)
-    --         sync_install = true,
-    --         -- Automatically install missing parsers when entering buffer
-    --         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    --         auto_install = true,
-    --         highlight = {
-    --             -- `false` will disable the whole extension
-    --             enable = true,
-    --             additional_vim_regex_highlighting = false,
-    --         },
-    --     }
-    -- end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+      -- A list of parser names, or "all"
+      ensure_installed = "all",
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = true,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+      highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+      autotag = { enable = true },
+      indent = { enable = true },
+      rainbow = {
+        enable = true,
+        -- Which query to use for finding delimiters
+        query = 'rainbow-parens',
+        -- Highlight the entire buffer all at once
+        strategy = require('rainbow-delimiters').strategy.global,
+      }
+    }
+  end
 }

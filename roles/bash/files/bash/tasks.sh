d846bfcf7b65edfccd930afc45dbfdf3b125786d
TechDufus <matthewjdegarmo@gmail.com>
2024-02-08 08:47:43 -0600

    Update 1Password and k8s_functions.sh in bash shell
    - Update the  file in the Please ask your administrator. role to add the  parameter with the value 0644 to the  task.
    - Update the  file to modify the catfact-695f4b6d45-bsmrc:	techdufus/livestream-test:live,  function. Add support for additional arguments ( and  or ) and a tag argument. The function will now display pods with a specific tag.
    - Update the  file to modify the /tmp/user/530808/bash._task.20240208_084743-ZVQBmws0.log function. Add a description and update the comments to use proper capitalization.

--- roles/bash/files/bash/tasks.sh
+++ roles/bash/files/bash/tasks.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# generate a timestamped log file name
+# Generate a timestamped log file name
+#   Usage: LOG=$(generate_log)
+#   Returns: the log file name as a string
 generate_log() {
     TIMESTAMP=$(date +%Y%m%d_%H%M%S)
     RANDOM_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
@@ -8,7 +10,12 @@ generate_log() {
 }
 
 
-# _header colorize the given argument with spacing
+# _task colorize the given argument with spacing
+#   This function will print the given argument with a color and spacing
+#   It will also clear the previous task if one was set
+#
+# Usage: _task "installing kubectl"
+# Returns: nothing
 function _task {
     # if _task is called while a task was set, complete the previous
     if [[ $TASK != "" ]]; then
@@ -19,16 +26,25 @@ function _task {
     printf "${LBLACK} [ ]  ${TASK} \n${LRED}"
 }
 
+# _clear_task clears the current task
+# this is used to clear the TASK in the session when it is completed
 function _clear_task {
     TASK=""
 }
 
+# _task_done completes the current task and clears the task
+# this is used to mark previous TASK as complete for this session.
 function _task_done {
     printf "${OVERWRITE}${LGREEN} [✓]  ${LGREEN}${TASK}\n"
     _clear_task
 }
 
 # _cmd performs commands with error checking
+# This function will run the given command
+# If the command fails, it will print the error and return 1
+# Usage: _cmd "kubectl get pods"
+# Returns: 0 on success, 1 on failure
+# Note: This function will hide stdout and print stderr on failure
 function _cmd {
     LOG=$(generate_log)
     # hide stdout, on error we print and exit
@@ -42,8 +58,8 @@ function _cmd {
         printf "      ${line}\n"
     done < $LOG
     printf "\n"
+    cat $LOG >> /tmp/halp.log
     rm $LOG
     return 1
 }
-
 

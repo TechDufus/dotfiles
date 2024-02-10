273675d99e038afa5edd126703e522e605efc0f1
TechDufus <matthewjdegarmo@gmail.com>
2024-02-10 15:29:51 -0600

    fix(neovim): Notifications are jank without this.

--- roles/neovim/files/lua/plugins/noice.lua
+++ roles/neovim/files/lua/plugins/noice.lua
@@ -10,7 +10,7 @@ return {
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        -- "rcarriga/nvim-notify",
+        "rcarriga/nvim-notify",
     },
     config = {
         lsp = {
@@ -22,7 +22,7 @@ return {
             },
         },
         messages = {
-            enabled = true
+            enabled = false
         },
         -- you can enable a preset for easier configuration
         presets = {

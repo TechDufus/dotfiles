86eb1972bc51bb479ea4a8000b862b818d0e44f6
TechDufus <matthewjdegarmo@gmail.com>
2024-02-26 10:24:33 -0600

    feat(neovim): Adding todo-comment plugin.

--- roles/neovim/files/lua/plugins/init.lua
+++ roles/neovim/files/lua/plugins/init.lua
@@ -1,32 +1,34 @@
 return {
-    "nvim-lua/popup.nvim",   -- An implementation of the Popup API from vim in Neovim
-    "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-    'simrat39/symbols-outline.nvim',
-    "ThePrimeagen/harpoon",
-    { "windwp/nvim-ts-autotag", dependencies = "nvim-treesitter" },
-    "neovim/nvim-lspconfig",
-    ({ "mfussenegger/nvim-dap", dependencies = { "rcarriga/nvim-dap-ui" } }),
-    "leoluz/nvim-dap-go",
-    "sharkdp/fd",
-    {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-        end
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-        lazy = false,
-    },
-    {
-        "sbdchd/neoformat",
-    },
-    -- {
-    --     'akinsho/bufferline.nvim',
-    --     version = "*",
-    --     dependencies = 'nvim-tree/nvim-web-devicons',
-    --     config = true,
-    -- },
-    'crispgm/nvim-go',
-    'erikzaadi/vim-ansible-yaml',
+  "nvim-lua/popup.nvim",   -- An implementation of the Popup API from vim in Neovim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+  'simrat39/symbols-outline.nvim',
+  "ThePrimeagen/harpoon",
+  { "windwp/nvim-ts-autotag", dependencies = "nvim-treesitter" },
+  "neovim/nvim-lspconfig",
+  ({ "mfussenegger/nvim-dap", dependencies = { "rcarriga/nvim-dap-ui" } }),
+  "leoluz/nvim-dap-go",
+  "sharkdp/fd",
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    "sbdchd/neoformat",
+  },
+  -- {
+  --     'akinsho/bufferline.nvim',
+  --     version = "*",
+  --     dependencies = 'nvim-tree/nvim-web-devicons',
+  --     config = true,
+  -- },
+  'crispgm/nvim-go',
+  'erikzaadi/vim-ansible-yaml',
 }

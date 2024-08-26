5e377c00365f75a213bcb90e7728c0ddd558e2fe
TechDufus <matthewjdegarmo@gmail.com>
2024-08-25 20:15:11 -0500

    Adding avante plugin. dadgum.

---
+++ roles/neovim/files/lua/plugins/avante.lua
@@ -0,0 +1,23 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  build = "make",
+  opts = {
+    -- add any opts here
+    provider = "openai",
+  },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below is optional, make sure to setup it properly if you have lazy=true
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}

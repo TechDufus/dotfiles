8300c2ce4f0e55113c869f15b8c739054b2e3745
TechDufus <matthewjdegarmo@gmail.com>
2024-09-12 11:52:41 -0500

    feat: Adding sudo, fixing misc issues.

--- roles/bash/files/os/Darwin/zz_os_functions.sh
+++ roles/bash/files/os/Darwin/zz_os_functions.sh
@@ -3,15 +3,12 @@
 alias update='brew update && brew upgrade && brew cleanup'
 
 # Homebrew completions
-if type brew &>/dev/null
-then
+if type brew &>/dev/null ; then
   HOMEBREW_PREFIX="$(brew --prefix)"
-  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
-  then
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
     source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
   else
-    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
-    do
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
       [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
     done
   fi

f4e02c4d2796afc515a976254e5002b0b48989a9
TechDufus <matthewjdegarmo@gmail.com>
2024-10-04 08:35:14 -0500

    feat: New git aliases.

--- roles/zsh/files/zsh/git_aliases.zsh
+++ roles/zsh/files/zsh/git_aliases.zsh
@@ -3,7 +3,10 @@
 alias gs='git status'
 alias gcane='git commit --amend --no-edit'
 alias gc="git checkout"
+alias gcm="git commit -m"
 alias gd="git diff"
+alias unstage="git restore --staged"
+alias gu='unstage'
 
 alias ggl="git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all"
 

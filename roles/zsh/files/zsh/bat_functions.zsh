ae4cc32678cb495e166fe8ca1ae0069c2122f0bc
TechDufus <matthewjdegarmo@gmail.com>
2024-10-24 08:57:09 -0500

    fix: ❓WHY was this removed?

--- roles/zsh/files/zsh/bat_functions.zsh
+++ roles/zsh/files/zsh/bat_functions.zsh
@@ -7,15 +7,15 @@
 # Example: kubectl logs some-pod | btail
 # Usage: btail [OPTION]... [FILE]...
 # Usage: echo "Hello" | btail
-# function btail() {
-#   if [ ! -t 0 ]; then
-#     cat /dev/stdin | bat --paging=never --language=log
-#     return
-#   fi
-#   if [ -z "$1" ]; then
-#     echo -e "${ARROW} ${YELLOW}Usage: btail [OPTION]... [FILE]...${NC}"
-#     return 1
-#   fi
-#   # tail -f $@ | bat -P -l log # This is the short version
-#   tail -f $@ | bat --paging=never --language=log
-# }
+function btail() {
+  if [ ! -t 0 ]; then
+    cat /dev/stdin | bat --paging=never --language=log
+    return
+  fi
+  if [ -z "$1" ]; then
+    echo -e "${ARROW} ${YELLOW}Usage: btail [OPTION]... [FILE]...${NC}"
+    return 1
+  fi
+  # tail -f $@ | bat -P -l log # This is the short version
+  tail -f $@ | bat --paging=never --language=log
+}

bd66f45b2ac462aaf868605adc121911486d30f1
TechDufus <matthewjdegarmo@gmail.com>
2024-11-13 14:50:41 -0600

    nvim: Switching to copilot.

--- roles/neovim/files/lua/plugins/avante.lua
+++ roles/neovim/files/lua/plugins/avante.lua
@@ -1,24 +1,51 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  build = "make",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
-    provider = "openai",
-    openai = {
-      model = "gpt-4o-mini",
-    }
+    provider = "copilot",
+    -- auto_suggestions_provider = "copilot",
+    -- openai = {
+    --   model = "gpt-4o-mini",
+    -- }
+    windows = {
+      input = {
+        prefix = "》"
+      },
+    },
   },
-  mappings = {
-    ask = "<leader>av",
-  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below is optional, make sure to setup it properly if you have lazy=true
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua",      -- for providers='copilot'
     {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
       opts = {
         file_types = { "markdown", "Avante" },

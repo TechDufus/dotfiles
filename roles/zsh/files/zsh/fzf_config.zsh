85ff89c23c0bcc3ea15306fa039b0fbb9187aa7d
TechDufus <matthewjdegarmo@gmail.com>
2024-12-19 11:34:00 -0600

    zsh: 》Adding catppuccin theme for fzf.

--- roles/zsh/files/zsh/fzf_config.zsh
+++ roles/zsh/files/zsh/fzf_config.zsh
@@ -3,6 +3,13 @@
 show_file_or_dir_preview="if [ -d {} ]; then lsd --oneline --tree --color=always --icon=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
 
 export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
+# catppuccin mocha theme.
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
 
 # Advanced customization of fzf options via _fzf_comprun function
 # - The first argument to the function is the name of the command.

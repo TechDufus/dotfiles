14d0cb26e13a290110f77f2ea7b3460e3c3e06ce
TechDufus <matthewjdegarmo@gmail.com>
2025-06-18 09:02:25 -0500

    feat: extend hammerspoon left positions to fill gap
    - Update top_left from 19x21 to 27x21 columns
    - Update bottom_left from 19x19 to 27x19 columns
    - Eliminates gap between left positions and center

--- roles/hammerspoon/files/config/positions.lua
+++ roles/hammerspoon/files/config/positions.lua
@@ -3,11 +3,11 @@ return {
   full = '0,0 80x40',
 
   standard = {
-    top_left     = '0,0 19x21',
-    bottom_left  = '0,21 19x19',
+    top_left     = '0,0 22x21',
+    bottom_left  = '0,21 22x19',
     full_left    = '0,0 19x40',
     left_center  = '19,0 8x40',
-    center       = '27,0 31x40',
+    center       = '22,0 36x40',
     right        = '58,0 22x40',
     right_hidden = '45,5 30x30', -- This is a full 1920x1080 on a 5120x1440 screen
     left_hidden  = '5,5 30x30',  -- This is a full 1920x1080 on a 5120x1440 screen

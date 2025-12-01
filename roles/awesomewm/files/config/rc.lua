3d9bd2b2838d9e3e99e1628757d3c0603030369d
TechDufus <matthewjdegarmo@gmail.com>
2025-11-30 23:15:07 -0600

    fix(awesomewm): disable sloppy focus
    Require click to focus windows instead of focus-follows-mouse.
    Prevents weird behavior with fullscreen apps and summoned windows.

--- roles/awesomewm/files/config/rc.lua
+++ roles/awesomewm/files/config/rc.lua
@@ -613,10 +613,10 @@ client.connect_signal("request::titlebars", function(c)
   }
 end)
 
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-  c:emit_signal("request::activate", "mouse_enter", { raise = false })
-end)
+-- Disable sloppy focus (focus follows mouse) - require click to focus
+-- client.connect_signal("mouse::enter", function(c)
+--   c:emit_signal("request::activate", "mouse_enter", { raise = false })
+-- end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)

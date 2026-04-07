0d094dc344176735a782c43b14c2dd8a756b51be
TechDufus <matthewjdegarmo@gmail.com>
2026-04-07 16:22:03 -0500

    fix(neovim): guard rainbow delimiters on parserless buffers

--- roles/neovim/files/lua/plugins/rainbow-delimiters.lua
+++ roles/neovim/files/lua/plugins/rainbow-delimiters.lua
@@ -1,4 +1,14 @@
 return {
   "HiPhish/rainbow-delimiters.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("rainbow-delimiters.setup").setup({
+      condition = function(bufnr)
+        -- Skip synthetic buffers that advertise a filetype but have no parser,
+        -- such as mini.files' `minifiles` buffers.
+        local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+        return ok and parser ~= nil
+      end,
+    })
+  end,
 }
